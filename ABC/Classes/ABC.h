0cb735c179dfcbb84c36f11ce0e3820aeb2b08ff
SRS <sirusheng@agora.io>
2020-12-23 14:21:48 +0800

    init

---
+++ ABC/Classes/ABC.h
@@ -0,0 +1,16 @@
+//
+//  ABC.h
+//  ABC
+//
+//  Created by SRS on 2020/12/23.
+//
+
+#import <Foundation/Foundation.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface ABC : NSObject
+- (void)abc;
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ ABC/Classes/ABC.m
@@ -0,0 +1,14 @@
+//
+//  ABC.m
+//  ABC
+//
+//  Created by SRS on 2020/12/23.
+//
+
+#import "ABC.h"
+
+@implementation ABC
+- (void)abc {
+    NSLog(@"abc");
+}
+@end
